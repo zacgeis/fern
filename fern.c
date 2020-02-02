cd050bd08948ad9ecf6a4847683e49c3fc5792fa
zacgeis <zacgeis@gmail.com>
2020-02-01 22:13:36 -0800

    Starting tokenizer.

--- fern.c
+++ fern.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef enum {
-  NUMBER_LITERAL,
-  PLUS,
-} Token;
-
-void PrintError(const char* message) {
-  printf("[ERROR] %s\n", message);
-}
+/*
+   Utility functions
+*/
 
 char* ReadFile(const char* filename) {
   FILE* file_handle = fopen(filename, "r");
   if (file_handle == NULL) {
-    PrintError("Failed to open file.");
+    printf("[ERROR] Failed to open file.\n");
     return NULL;
   }
 
@@ -24,7 +20,7 @@ char* ReadFile(const char* filename) {
   char* buffer = (char*) malloc(sizeof(char) * (file_size + 1));
 
   if (buffer == NULL) {
-    PrintError("Failed to allocate buffer.");
+    printf("[ERROR] Failed to allocate buffer.\n");
     return NULL;
   }
 
@@ -33,7 +29,7 @@ char* ReadFile(const char* filename) {
   buffer[file_size] = '\0';
 
   if (read_size != file_size) {
-    PrintError("Failed to read expected size.");
+    printf("[ERROR] Failed to read expected size.\n");
     free(buffer);
     return NULL;
   }
@@ -43,14 +39,85 @@ char* ReadFile(const char* filename) {
   return buffer;
 }
 
+char* SubStr(char* base, int i, int len) {
+  // Create a new str with one additional char for the terminating '\0'.
+  char* str = (char*) malloc((len + 1) * sizeof(char));
+  memcpy(str, base + i, len * sizeof(char));
+  str[len] = '\0';
+  return str;
+}
+
+/*
+  Tokenizer
+*/
+
+typedef enum {
+  kIntLiteral,
+  kPlus,
+  kMinus,
+} TokenType;
+
+typedef struct {
+  TokenType type;
+  char* value;
+} Token;
+
+typedef struct {
+  Token* tokens;
+  int length;
+  int capacity;
+} TokenList;
+
+Token* TokenListAppend(TokenList* list) {
+  if (list->length == list->capacity) {
+    list->capacity *= 2;
+    list->tokens = (Token*) realloc(list->tokens, list->capacity * sizeof(Token));
+  }
+  return &list->tokens[list->length++];
+}
+
+TokenList Tokenize(char* input) {
+  TokenList list;
+  list.length = 0;
+  list.capacity = 100;
+  list.tokens = (Token*) malloc(list.capacity * sizeof(Token));
+
+  Token* token = NULL;
+  for (int i = 0; input[i] != '\0'; i++) {
+    char c = input[i];
+    if (c == ' ' || c == '\n' || c == '\t') {
+      continue;
+    } else if (c == '+') {
+      token = TokenListAppend(&list);
+      token->type = kPlus;
+      token->value = SubStr(input, i, 1);
+    } else if (c == '1') {
+      token = TokenListAppend(&list);
+      token->type = kIntLiteral;
+      token->value = SubStr(input, i, 1);
+    } else {
+      printf("[ERROR] Bad token.\n");
+      // TODO: Add pretty printing of the token in the string here.
+    }
+  }
+
+  return list;
+}
+
+/*
+   Main
+*/
+
 int main(int argc, char** argv) {
   if (argc < 2) {
-    PrintError("No filed provided.");
+    printf("[ERROR] No filed provided.\n");
     return -1;
   }
 
   char* input = ReadFile(argv[1]);
-  printf("Contents: %s\n", input);
+  TokenList token_list = Tokenize(input);
+  printf("Token list length: %d\n", token_list.length);
+  printf("Plus token: %s\n", token_list.tokens[1].value);
   free(input);
 
   return 0;
